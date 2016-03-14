8f655cda390720dd74e43194e20347a7a8649294
shasa1 <shasalman746@gmail.com.com>
2016-03-14 12:10:59 +0530

    first commit

---
+++ src/test/java/com/sample/SampleTest.java
@@ -0,0 +1,34 @@
+package com.sample;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+
+
+public class SampleTest {
+	WebDriver driver;
+
+	@BeforeMethod
+	public void setUp() throws Exception{
+		System.setProperty("webdriver.chrome.driver", "C:/Documents and Settings/admin/My Documents/Downloads/chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.get("http://www.google.com");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+	}
+	@Test
+	public void testTitle(){
+		Assert.assertEquals("goog",driver.getTitle(),"The Title of the FirstPage is not correct");
+	}
+	@AfterMethod
+	public void tearDown(){
+		driver.close();
+		driver.quit();	}
+}
+
